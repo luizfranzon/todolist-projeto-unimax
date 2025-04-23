0b568640e1158dc001d22097bbd8cf62a54837b5
Luiz Franzon <luizfranzon05@hotmail.com>
2025-04-23 00:20:05 +0000

    WIP: creation tab

--- todolist/App.tsx
+++ todolist/App.tsx
@@ -1,15 +1,13 @@
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import {
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  View,
-  Text,
-} from "react-native";
-import { ListItem } from "./components/ListItem";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 
+const Tab = createBottomTabNavigator();
+
+import { TasksPage } from "./pages/TasksPage";
+import { CompletedTasks } from "./pages/CompletedTasks";
 export interface Task {
   id: string;
   title: string;
@@ -17,26 +15,14 @@ export interface Task {
 }
 
 export default function App() {
-  const [typedText, setTypedText] = useState("");
   const [tasks, setTasks] = useState<Task[]>([]);
-
-  function handleAddTask() {
-    if (typedText.trim() === "") {
-      return;
-    }
-
-    const newTask: Task = {
-      id: String(new Date().getTime()),
-      title: typedText,
-      isCompleted: false,
-    };
-
-    setTasks((prevState) => [...prevState, newTask]);
-    setTypedText("");
-  }
 
   function onDelete(id: string) {
     setTasks((prevState) => prevState.filter((task) => task.id !== id));
+  }
+
+  function onAddTask(newTask: Task) {
+    setTasks((prevState) => [...prevState, newTask]);
   }
 
   function onCheck(id: string, isCompleted: boolean) {
@@ -49,78 +35,25 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={styles.main}>
+      <SafeAreaView>
         <StatusBar style="light" />
-        <View style={styles.container}>
-
-          <TextInput
-            style={styles.input}
-            onChangeText={setTypedText}
-            value={typedText}
-            placeholder="Digite o nome da tarefa"
-            placeholderTextColor="#7A7A80"
-          />
-
-          <TouchableOpacity onPress={handleAddTask} style={styles.button}>
-            <Text style={styles.buttonText}>Adicionar tarefa</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.itemsList}>
-          {tasks.map((task) => (
-            <ListItem
-              key={task.id}
-              title={task.title}
-              id={task.id}
-              isCompleted={task.isCompleted}
-              onDelete={onDelete}
-              onCheck={onCheck}
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Tarefas Pendentes"
+              component={() => (
+                <TasksPage
+                  onAddTask={onAddTask}
+                  onCheck={onCheck}
+                  onDelete={onDelete}
+                  tasks={tasks.filter((task) => !task.isCompleted)}
+                />
+              )}
             />
-          ))}
-        </View>
+            <Tab.Screen name="Tarefas Concluídas" component={CompletedTasks} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  main: {
-    backgroundColor: "#141415",
-    flex: 1,
-  },
-  container: {
-    backgroundColor: "#141415",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 24,
-  },
-  input: {
-    paddingHorizontal: 10,
-    borderColor: "#007AFF",
-    color: "white",
-    borderRadius: 12,
-    borderWidth: 1,
-    width: "90%",
-    height: 58,
-    fontSize: 16,
-  },
-  button: {
-    backgroundColor: "#007AFF",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 12,
-    marginTop: 10,
-    width: "90%",
-    padding: 10,
-    height: 58,
-  },
-  buttonText: {
-    color: "white",
-  },
-  itemsList: {
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
-    marginTop: 24,
-  },
-});
---
+++ todolist/pages/CompletedTasks.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function CompletedTasks() {
+  return (
+    <View>
+      <Text>Completed Tasks</Text>
+    </View>
+  );
+}
